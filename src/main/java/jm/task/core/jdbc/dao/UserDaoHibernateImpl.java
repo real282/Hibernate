4038bc155735f1bebc4437f759afda560005c79d
Admin <real282@mail.ru>
2021-08-12 00:42:14 +0800

    На проверку

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -5,23 +5,37 @@ import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-import java.util.List;
+import java.util.*;
 
-public class UserDaoHibernateImpl implements UserDao{
+public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
 
     }
 
-
     @Override
     public void createUsersTable() {
-        //Session session = Util.getSessionFactory().openSession();
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE users (id INT AUTO_INCREMENT, " +
+                    "name VARCHAR(30), " +
+                    "lastName VARCHAR(30), " +
+                    "age TINYINT, " +
+                    "PRIMARY KEY (id))").executeUpdate();
+            transaction.commit();
+        } catch (Exception ignore) {
 
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery("DROP TABLE users").executeUpdate();
+            transaction.commit();
+        } catch (Exception ignore) {
 
+        }
     }
 
     @Override
@@ -35,16 +49,32 @@ public class UserDaoHibernateImpl implements UserDao{
 
     @Override
     public void removeUserById(long id) {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            User user = (User) session.get(User.class, id);
+            Transaction tx = session.beginTransaction();
+            session.delete(user);
+            tx.commit();
+        } catch (IllegalArgumentException ignore) {
 
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Session session = Util.getSessionFactory().openSession();
+        List<User> users = session.createQuery("from User").list();
+        session.close();
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.createQuery("DELETE FROM User").executeUpdate();
+            tx.commit();
+        } catch (Exception ignore) {
 
+        }
     }
 }
