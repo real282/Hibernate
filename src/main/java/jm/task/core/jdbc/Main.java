2179a9d0e9b2999a3948944eef73eb6f288a02e1
Evgeny Roznin <real282@mail.ru>
2021-08-11 18:23:12 +0800

    ???

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -10,10 +10,10 @@ public class Main {
     public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
         //userService.createUsersTable();
-        userService.saveUser("Petr", "Petrov", (byte) 20);
+       /* userService.saveUser("Petr", "Petrov", (byte) 20);
         userService.saveUser("Ivan", "Ivanov", (byte) 15);
         userService.saveUser("Kola", "Sidorov", (byte) 30);
-        userService.saveUser("Nikita", "Nikitin", (byte) 23);
+        userService.saveUser("Nikita", "Nikitin", (byte) 23);*/
         //List<User> user = userService.getAllUsers();
         //System.out.println(user.toString());
         //userService.cleanUsersTable();
